90419c4c6261b6dd7e6d1b78722397200e1e8537
Levan <levanlom96@gmail.com>
2023-05-07 22:41:06 +0200

    changes everything

--- JS/main.js
+++ JS/main.js
@@ -20,3 +20,17 @@ for(let i = 0; i < title.length; i++) {
     observer.observe(title[i]);
     observer.observe(text[i]);
 }
+
+// Buy now button
+const button = document.querySelector('.buy-now-btn');
+const offset = button.offsetTop + 70;
+
+function toggleButtonFixed() {
+    if (window.pageYOffset > offset) {
+        button.classList.add('fixed');
+    } else {
+        button.classList.remove('fixed');
+    }
+}
+
+window.addEventListener('scroll', toggleButtonFixed);
